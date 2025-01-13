4fb28dc1f05e8f1fe0290d7b592b189fa33485db
Meg White <dearmegwhite@gmail.com>
2025-01-13 13:32:59 +1000

    Create extract_and_prepare_data.sh

---
+++ processing_scripts/extract_and_prepare_data.sh
@@ -0,0 +1,6 @@
+python3 sg_infodump.py
+python3 se_infodump.py
+python3 network_infodump.py
+python3 network_agg.py
+mv Aggregated_Network_Data_Summary.csv Aggregated_Network_Data.csv
+python3 combine_data.py
